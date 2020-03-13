544c7bf327bd70ad39ac49551f83ad028c06f28f
João Marcos <50593874+joaobem@users.noreply.github.com>
2020-03-13 11:42:43 -0300

    Add files via upload

--- java/funcoes.js
+++ java/funcoes.js
@@ -1,7 +1,7 @@
 alert("arquivo aberto")
 console.log("Arquivos JS lido")
 
-function numerolegal (valor) {
+function numeroLegal (valor) {
     //se o valor for maior que 100
     return valor % 2 == 0 && valor > 100
 }
@@ -20,3 +20,31 @@ console.log ('Numero Chato - 66-, 76, 77')
 console.log(numeroChato(66))
 console.log(numeroChato(76))
 console.log(numeroChato(77))
+
+var frutas  = ["banana", "pera", "laranja", "kiwi"]
+for (fruta of frutas) {
+    console.log(fruta)
+}
+console.log(frutas.indexOf("pera"))
+console.log(frutas.indexOf("laranja"))
+console.log(frutas.indexOf("maçã"))
+
+console.log('usando for')
+for( var i=0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('usando while')
+var i = 0
+while (i < 10) {
+    console.log(i)
+    i += 1
+}
+
+function mostrarData() {
+    console.log(new Date())
+}
+
+mostrarData()
+//setInterval(mostrarData, 2000)
+setTimeout(mostrarData, 2000)
