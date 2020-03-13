ece0decb746e01f264576a33d2446670766a5678
João Marcos <50593874+joaobem@users.noreply.github.com>
2020-03-13 11:47:25 -0300

    Add files via upload

---
+++ java/atividade.js
@@ -0,0 +1,11 @@
+alert("arquivo aberto")
+console.log("Exercícios de Java Script")
+
+var endereco = { 
+    rua: "Rua dos Príncipes",  
+    numero: 123,  
+    bairro: "Centro",  
+    cidade: "Joinville",  
+    uf: "SC"
+    }
+    return endereço
